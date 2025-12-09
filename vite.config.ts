4ccbbf84576c7a0471de84262a19961bd11d4718
litdu <huxofi@gmail.com>
2025-12-09 15:59:54 +0000

    fix: configure vite base for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoBase = '/Zlowy/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: repoBase,
   plugins: [react()],
 })
